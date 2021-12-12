fffad3e34f904d528cac51d74afcf97366e12793
Exem <chernyakov.ghost@mail.ru>
2021-12-12 18:50:32 +0500

    WORKING

--- rgzTU/Form1.Designer.cs
+++ rgzTU/Form1.Designer.cs
@@ -29,10 +29,10 @@ namespace rgzTU
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
@@ -52,37 +52,43 @@ namespace rgzTU
             this.startBTN = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.integratorTB = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.powering2TB = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 148);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            series4.Legend = "Legend1";
-            series4.Name = "Series2";
-            this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(449, 181);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(938, 479);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(502, 9);
+            this.label1.Location = new System.Drawing.Point(1012, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 13);
             this.label1.TabIndex = 1;
@@ -90,7 +96,7 @@ namespace rgzTU
             // 
             // poweringTB
             // 
-            this.poweringTB.Location = new System.Drawing.Point(570, 6);
+            this.poweringTB.Location = new System.Drawing.Point(1080, 13);
             this.poweringTB.Name = "poweringTB";
             this.poweringTB.Size = new System.Drawing.Size(76, 20);
             this.poweringTB.TabIndex = 2;
@@ -99,7 +105,7 @@ namespace rgzTU
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(467, 60);
+            this.label2.Location = new System.Drawing.Point(977, 67);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 3;
@@ -107,7 +113,7 @@ namespace rgzTU
             // 
             // k1TB
             // 
-            this.k1TB.Location = new System.Drawing.Point(546, 43);
+            this.k1TB.Location = new System.Drawing.Point(1080, 51);
             this.k1TB.Name = "k1TB";
             this.k1TB.Size = new System.Drawing.Size(100, 20);
             this.k1TB.TabIndex = 4;
@@ -115,7 +121,7 @@ namespace rgzTU
             // 
             // t1TB
             // 
-            this.t1TB.Location = new System.Drawing.Point(546, 69);
+            this.t1TB.Location = new System.Drawing.Point(1080, 77);
             this.t1TB.Name = "t1TB";
             this.t1TB.Size = new System.Drawing.Size(100, 20);
             this.t1TB.TabIndex = 5;
@@ -123,7 +129,7 @@ namespace rgzTU
             // 
             // t2TB
             // 
-            this.t2TB.Location = new System.Drawing.Point(546, 130);
+            this.t2TB.Location = new System.Drawing.Point(1080, 138);
             this.t2TB.Name = "t2TB";
             this.t2TB.Size = new System.Drawing.Size(100, 20);
             this.t2TB.TabIndex = 8;
@@ -131,7 +137,7 @@ namespace rgzTU
             // 
             // k2TB
             // 
-            this.k2TB.Location = new System.Drawing.Point(546, 104);
+            this.k2TB.Location = new System.Drawing.Point(1080, 112);
             this.k2TB.Name = "k2TB";
             this.k2TB.Size = new System.Drawing.Size(100, 20);
             this.k2TB.TabIndex = 7;
@@ -140,7 +146,7 @@ namespace rgzTU
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(467, 121);
+            this.label3.Location = new System.Drawing.Point(977, 128);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(73, 13);
             this.label3.TabIndex = 6;
@@ -149,7 +155,7 @@ namespace rgzTU
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(513, 163);
+            this.label4.Location = new System.Drawing.Point(1023, 170);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(27, 13);
             this.label4.TabIndex = 9;
@@ -158,7 +164,7 @@ namespace rgzTU
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(502, 194);
+            this.label5.Location = new System.Drawing.Point(1012, 201);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(40, 13);
             this.label5.TabIndex = 10;
@@ -167,7 +173,7 @@ namespace rgzTU
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(473, 222);
+            this.label6.Location = new System.Drawing.Point(983, 229);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(69, 13);
             this.label6.TabIndex = 11;
@@ -175,15 +181,15 @@ namespace rgzTU
             // 
             // stepTB
             // 
-            this.stepTB.Location = new System.Drawing.Point(546, 160);
+            this.stepTB.Location = new System.Drawing.Point(1080, 168);
             this.stepTB.Name = "stepTB";
             this.stepTB.Size = new System.Drawing.Size(100, 20);
             this.stepTB.TabIndex = 12;
-            this.stepTB.Text = "0,0001";
+            this.stepTB.Text = "0,001";
             // 
             // timeTB
             // 
-            this.timeTB.Location = new System.Drawing.Point(546, 191);
+            this.timeTB.Location = new System.Drawing.Point(1080, 199);
             this.timeTB.Name = "timeTB";
             this.timeTB.Size = new System.Drawing.Size(100, 20);
             this.timeTB.TabIndex = 13;
@@ -191,7 +197,7 @@ namespace rgzTU
             // 
             // controlTB
             // 
-            this.controlTB.Location = new System.Drawing.Point(546, 219);
+            this.controlTB.Location = new System.Drawing.Point(1080, 227);
             this.controlTB.Name = "controlTB";
             this.controlTB.Size = new System.Drawing.Size(100, 20);
             this.controlTB.TabIndex = 14;
@@ -199,7 +205,7 @@ namespace rgzTU
             // 
             // startBTN
             // 
-            this.startBTN.Location = new System.Drawing.Point(546, 257);
+            this.startBTN.Location = new System.Drawing.Point(1056, 354);
             this.startBTN.Name = "startBTN";
             this.startBTN.Size = new System.Drawing.Size(100, 23);
             this.startBTN.TabIndex = 15;
@@ -219,17 +225,74 @@ namespace rgzTU
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(500, 314);
+            this.label7.Location = new System.Drawing.Point(1008, 364);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(35, 13);
             this.label7.TabIndex = 17;
             this.label7.Text = "label7";
             // 
+            // integratorTB
+            // 
+            this.integratorTB.Location = new System.Drawing.Point(1080, 253);
+            this.integratorTB.Name = "integratorTB";
+            this.integratorTB.Size = new System.Drawing.Size(100, 20);
+            this.integratorTB.TabIndex = 18;
+            this.integratorTB.Text = "1,43";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(983, 255);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Интегратор";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1080, 278);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.Text = "0,175";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(977, 281);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Дифференциатор";
+            // 
+            // powering2TB
+            // 
+            this.powering2TB.Location = new System.Drawing.Point(1080, 310);
+            this.powering2TB.Name = "powering2TB";
+            this.powering2TB.Size = new System.Drawing.Size(100, 20);
+            this.powering2TB.TabIndex = 22;
+            this.powering2TB.Text = "0,6";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(977, 313);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Усилитель 2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(660, 341);
+            this.ClientSize = new System.Drawing.Size(1231, 640);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.powering2TB);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.integratorTB);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.startBTN);
@@ -277,6 +340,12 @@ namespace rgzTU
         private System.Windows.Forms.Button startBTN;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox integratorTB;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox powering2TB;
+        private System.Windows.Forms.Label label10;
     }
 }
 
--- rgzTU/Form1.cs
+++ rgzTU/Form1.cs
@@ -17,45 +17,93 @@ namespace rgzTU
             InitializeComponent();
         }
 
+        
+
         private void startBTN_Click(object sender, EventArgs e)
         {
-            double xPast = 0, epsPast = 0, UyPast = 0, yPast = 0, UaPast = 0, Uak;
+            double[] mas1 = new double[Convert.ToInt32(Convert.ToInt32(timeTB.Text) / Convert.ToDouble(stepTB.Text))];
+            double[] mas2 = new double[Convert.ToInt32(Convert.ToInt32(timeTB.Text) / Convert.ToDouble(stepTB.Text))];
+            double[] mas3 = new double[Convert.ToInt32(Convert.ToInt32(timeTB.Text) / Convert.ToDouble(stepTB.Text))];
+            double[] mas4 = new double[Convert.ToInt32(Convert.ToInt32(timeTB.Text) / Convert.ToDouble(stepTB.Text))];
 
-            for(double t = 0; t < Convert.ToDouble(timeTB.Text); t = t + Convert.ToDouble(stepTB.Text))
+            mas1[0] = 0;
+            mas2[0] = 0;
+            int iterator = 1;
+
+            double xPast = Convert.ToDouble(controlTB.Text), /**/ epsPast = 0/*выход сумматорп*/, UyPast = 0/*выход исилителя*/, yPast = 0/**/, UaPast = 0/*Выход усилителя*/, Uak = 0/*Выход апериодического*/;
+
+            for (double t = Convert.ToDouble(stepTB.Text); t < Convert.ToDouble(timeTB.Text); t = t + Convert.ToDouble(stepTB.Text))
             {
-                xPast = t;
+                //xPast = Convert.ToDouble(controlTB.Text);
                 epsPast = xPast - yPast; // ЦМ сумматора
+                //Console.WriteLine($"Время {t}: epsPast={epsPast} ");
+                        
+                //Console.WriteLine($"Время {t}: UyPast={UyPast} ");
 
-                //chart1.Series[0].Points.AddXY(t, epsPast);
+                double f = Convert.ToDouble(poweringTB.Text) * epsPast;
+                //chart1.Series[0].Points.AddXY(t, f);
+                if (iterator != Convert.ToInt32(Convert.ToInt32(timeTB.Text) / Convert.ToDouble(stepTB.Text)))
+                {
+                    mas3[iterator] = t;
+                    mas4[iterator] = f;
+                }
+                double shit = integrator(yPast, Convert.ToDouble(stepTB.Text), Convert.ToDouble(integratorTB.Text), UaPast);
+                double poop = dx(Convert.ToDouble(powering2TB.Text), Convert.ToDouble(stepTB.Text), UaPast, Uak);
+                double secondDifference = (
+                    f
+                    -
+                    (
+                        shit
+                        -
+                        poop
+                    ));
 
-                UyPast = Convert.ToDouble(poweringTB.Text) * epsPast; //ЦМ усилительного звена
-                
+                UyPast = Convert.ToDouble(poweringTB.Text) * secondDifference; //ЦМ усилительного звена
+
                 Uak = aperValue( //ЦМ 1 апериодического звена
                     UaPast,
                     Convert.ToDouble(stepTB.Text),
                     Convert.ToDouble(k1TB.Text),
                     Convert.ToDouble(t1TB.Text),
                     UyPast);
-                
-                Uak = aperValue( //ЦМ 2 апериодического звена
-                    Uak,
+
+                Console.WriteLine($"Время {t}: Uak1={Uak} ");
+
+                double newUak = aperValue( //ЦМ 2 апериодического звена
+                Uak,
                     Convert.ToDouble(stepTB.Text),
                     Convert.ToDouble(k2TB.Text),
                     Convert.ToDouble(t2TB.Text),
                     UyPast);
 
+                //Console.WriteLine($"Время {t}: Uak2={Uak} ");
+
                 //chart1.Series[1].Points.AddXY(t, Uak);
-
-                yPast = Uak;
-                UaPast = Uak;
-
+                if(iterator != Convert.ToInt32(Convert.ToInt32(timeTB.Text) / Convert.ToDouble(stepTB.Text)))
+                {
+                    mas1[iterator] = t;
+                    mas2[iterator] = Uak;
+                    iterator++;
+                }
+                
+                yPast = newUak;
+                UaPast = newUak;
+                //Console.WriteLine($"time {t}, yPast={yPast}, UaPast={UaPast}");
             }
             //label7.Text = UaPast.ToString();
+
+            for(int i = 0; i < mas1.Length; i++)
+            {
+                chart1.Series[1].Points.AddXY(mas1[i], mas2[i]);
+                chart1.Series[0].Points.AddXY(mas3[i], mas4[i]);
+            }
+
+
+
         }
 
-        double aperValue(double UaPast, double step, double k, double t, double UyPast) => UaPast + step * ((k / t) * UyPast - UaPast / t); 
-
-
-
+        double aperValue(double UaPast, double step, double k, double t, double UyPast) => UaPast + step * ((k / t) * UyPast - UaPast / t);
+        double integrator(double Ypast, double deltaT, double K, double Upast) => Ypast + deltaT * K * Upast;
+        double dx(double K, double deltaT, double Ukpast, double Uk) => (K / deltaT) * (Uk - Ukpast);
     }
 }
